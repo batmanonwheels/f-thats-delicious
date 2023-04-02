0ab832df86e2566042ac30f9d1411f8815e93eb4
Devin Garcia <devingarcia5423@gmail.com>
2023-04-02 17:17:41 -0400

    added ruby backend

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { useCallback, useState } from 'react';
+import Map from './components/Map';
+import './App.css';
+
+export default function App() {
+	return (
+		<>
+			<Map />
+		</>
+	);
+}
---
+++ client/src/components/Map.tsx
@@ -0,0 +1,32 @@
+export default function Map() {
+	const initializeMap = async (): Promise<void> => {
+		//@ts-ignore
+		let map: google.maps.Map;
+
+		//@ts-ignore
+		const { Map } = await google.maps.importLibrary('maps');
+		map = new Map(document.getElementById('map') as HTMLElement, {
+			center: { lat: 40.712, lng: 74.006 },
+			zoom: 8,
+		});
+	};
+
+	initializeMap();
+
+	// const loader = new Loader({
+	// 	apiKey: 'AIzaSyCVnyyFCfdA7CUV9e_anjmBm_C0cxJtp80',
+	// 	version: 'weekly',
+	// });
+
+	// loader.load().then(async () => {
+	// 	// @ts-ignore google
+	// 	const { Map } = (await google.maps.importLibrary(
+	// 		'maps'
+	// 	)) as google.maps.MapsLibrary;
+	// 	let map = new Map(document.getElementById('map') as HTMLElement, {
+	// 		center: { lat: -34.397, lng: 150.644 },
+	// 		zoom: 8,
+	// 	});
+
+	return <div id='map'></div>;
+}
